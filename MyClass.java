6258e3d95b1c7ebe1c2f51458fb0cfab70adfdf5
geisyc93-ops <geisyc93@gmail.com>
2025-11-13 13:48:04 -0300

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,33 @@
+
+
+
+
+
+ 
+
+  
+
+  
+
+
+
+
+public class MyClass {
+  public static void main(String args[]) {
+    int x=10;
+    int y=25;
+    int z=x+y;
+    int 7,0  
+    int 6,0 
+    int 8,0
+    int 9,0
+    int 10,0
+    1o Bimestre: 6.0
+2o Bimestre: 7.0
+1o Semestre: 6.5
+----------------------
+3o Bimestre: 8.0
+4o Bimestre: 9.0
+2o Semestre: 8.5
+-----------------------
+Média Final: 7.5
